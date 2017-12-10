cdde22d8b20e7112d39df5e11c0e66685e471dcc
tayloraj10 <taylor.alan.johnson@gmail.com>
2017-12-10 14:11:45 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -108,4 +108,4 @@ def reset():
     return render_template('index.html', images=new_images)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
